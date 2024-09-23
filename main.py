00c88b27cf390c270e04f1a81b3448ee03053bdb
lou57810 <bernard.oswald@free.fr>
2024-09-23 17:35:59 +0200

    Ajust main, .gitignore

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def main():
         4: '4: Solution test optimisee  (algo dynamique)',
         5: '5: Solution optimisee (algo dynamique) Sienna1',
         6: '6: Solution optimisee (algo dynamique) Sienna2',
-        # 5: '5: Test Brut Force binaire Sienna1 (20 actions)',
-        # 7: '7: Test Brut Force Sienna2 binaire(panier restreint 20 actions)',
-        # 9: '9: Test recursif Sienna2(panier restreint 20 actions)',
+        7: '7: Test Brut Force binaire Sienna1 (20 actions)',
+        8: '8: Test Brut Force Sienna2 binaire(panier restreint 20 actions)',
+        9: '9: Test recursif Sienna2(panier restreint 20 actions)',
         0: '0: Exit',
     }
 
@@ -50,18 +50,18 @@ def main():
         sol_brut = OptiAlgo("CsvData/dataset2_Python+P7.csv",
                             "CsvData/dataset2.csv", 500)
         sol_brut.execute()
-    # elif option == 7:
-        # sol_brut = Binary("CsvData/dataset1_Python+P7_20.csv",
-        # "CsvData/dataset11.csv", 500)
-        # sol_brut.execute()
-    # elif option == 8:
-        # sol_brut = Binary("CsvData/dataset2_Python+P7_25.csv",
-        # "CsvData/dataset12.csv", 500)
-        # sol_brut.execute()
-    # elif option == 9:
-        # sol_brut = Recursiv("CsvData/dataset2_Python+P7_25.csv",
-        # "CsvData/dataset12.csv", 500)
-        # sol_brut.execute()
+    elif option == 7:
+        sol_brut = Binary("CsvData/dataset1_Python+P7_20.csv",
+        "CsvData/dataset11.csv", 500)
+        sol_brut.execute()
+    elif option == 8:
+        sol_brut = Binary("CsvData/dataset2_Python+P7_25.csv",
+        "CsvData/dataset12.csv", 500)
+        sol_brut.execute()
+    elif option == 9:
+        sol_brut = Recursiv("CsvData/dataset2_Python+P7_25.csv",
+        "CsvData/dataset12.csv", 500)
+        sol_brut.execute()
     elif option == 0:
         print('Sortie programme.')
         exit()
